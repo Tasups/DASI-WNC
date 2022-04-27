5ccb2172fa9eb2ea0a721a49b6d5a551d4ff9234
Jason Whisnant <tasups@gmail.com>
2022-04-27 20:48:03 +0000

    Add react-router-dom from https://reactrouter.com/docs/en/v6/getting-started/installation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from './components/Navbar.js'
+
+import Navbar from './components/Navbar.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <Navbar />
+    
       
     </div>
   );
