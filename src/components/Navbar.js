c39307de31224248a0c86a68a73f5ce919d4760f
Jason Whisnant <tasups@gmail.com>
2022-05-12 20:48:22 +0000

    hamburger nav implemented functionally, need to work on the aside nav and the backdrop

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFan } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
+  
+  const [isOpen, setIsOpen] = useState(false);
+  
+  const toggleOpen = () => {
+    setIsOpen(isOpen => !isOpen)
+  }
+  
+  
   return(
-    <div className="navbar-wrapper">
+    <nav className="navbar-wrapper">
+    
       <div className="navbar-container">
         <div className="navbar-logo-wrapper">
           <FontAwesomeIcon className="navbar-logo" icon={faFan} />
           <h1>DASI</h1>
         </div>
-      <ul className="navbar-links">
-        <li>
-          <Link to="/" className="navbar-links"><h3>HOME</h3></Link>
-        </li>
-        <li>
-          <Link to="/about" className="navbar-links"><h3>ABOUT</h3></Link>
-        </li>
-        <li>
-          <Link to="events&venues" className="navbar-links"><h3>EVENTS & VENUES</h3></Link>
-        </li>
-        <li>
-         <Link to="getinvolved" className="navbar-links"><h3>GET INVOLVED</h3></Link>
-        </li>
-      </ul>
-    </div>
-    </div>
+        <ul className="navbar-links">
+          <li>
+            <Link to="/" className="navbar-links"><h3>HOME</h3></Link>
+          </li>
+          <li>
+            <Link to="/about" className="navbar-links"><h3>ABOUT</h3></Link>
+          </li>
+          <li>
+            <Link to="events&venues" className="navbar-links"><h3>EVENTS & VENUES</h3></Link>
+          </li>
+          <li>
+           <Link to="getinvolved" className="navbar-links"><h3>GET INVOLVED</h3></Link>
+          </li>
+        </ul>
+      </div>
+      <div className="hamburger-nav">
+        <button className="nav-hamburger-btn" type="button" onClick={toggleOpen}>
+          <FontAwesomeIcon className="nav-hamburger-icon" icon={faBars} />
+        </button>
+        {isOpen && <h1>MOBILE NAV OPEN</h1>}
+      </div>
+    
+    </nav>
     
     )
 }
