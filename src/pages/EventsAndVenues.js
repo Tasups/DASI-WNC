9dffcbfb19b2c7389f5e69e3c9df258feec7906e
Jason Whisnant <tasups@gmail.com>
2025-10-30 10:05:40 -0400

    Revert "Merge pull request #10 from Tasups/add_pre-show_description-link"
    This reverts commit 2a47d8110d9071461b7ea6cc2a219a6e8ad939c3, reversing
    changes made to a55bf36edbebadebc49e390ff217f67eb717835f.

--- src/pages/EventsAndVenues.js
+++ src/pages/EventsAndVenues.js
@@ -58,10 +58,6 @@ const EventsAndVenues = () => {
                 <br></br>
                 <span className="event-info">{event.description}</span>
                 <br></br>
-                <span className="event-info-header">Pre-show Description: </span>
-                <br></br>
-                <span className="event-info">{event.preShowDescLink}</span>
-                <br></br>
                 <span className="event-info-header">Run Time: </span>
                 <br></br>
                 <span className="event-info">{event.runTime}</span>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import fetch from 'node-fetch'
+import React from 'react';
 
 const Home = () => {
-  const [data, setData] = useState()
-
-  useEffect(() => {
-    const responseData = async () => {
-      await fetch("https://www.dnd5eapi.co/api/spells/fireball/")
-        .then((response) => response.json())
-        .then((data) => setData(data));
-    };
-    responseData()
-  }, [])
-  
-  console.log(data)
-  
-  const name = data.name
-  
-
   return(
     <div className="home-container card">
       <ul>
         <li className="card-line-item">
           <h2>
-            {name}
             The mission of Descriptive Audio for the Sight Impaired (DASI) is to provide audio description for live theater and other events in the Asheville, NC area.
           </h2>
         </li>
