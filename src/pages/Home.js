f90af5375ea0047011f3de36b898f7a5ba792a17
Jason Whisnant <tasups@gmail.com>
2025-10-30 09:57:26 -0400

    Add pre-show description link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import fetch from 'node-fetch'
 
 const Home = () => {
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    const responseData = async () => {
+      await fetch("https://www.dnd5eapi.co/api/spells/fireball/")
+        .then((response) => response.json())
+        .then((data) => setData(data));
+    };
+    responseData()
+  }, [])
+  
+  console.log(data)
+  
+  const name = data.name
+  
+
   return(
     <div className="home-container card">
       <ul>
         <li className="card-line-item">
           <h2>
+            {name}
             The mission of Descriptive Audio for the Sight Impaired (DASI) is to provide audio description for live theater and other events in the Asheville, NC area.
           </h2>
         </li>
